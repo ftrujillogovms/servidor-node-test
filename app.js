49a497cb4f41614b307f78720d8fae77b0464a47
felipe <ftrujillo@govms.cl>
2022-06-20 18:51:45 -0400

    produccion

--- app.js
+++ app.js
@@ -1,11 +1,69 @@
 const express = require('express')
+var fs = require('fs')
+const multer = require('multer')
+const router = express.Router()
+const util = require('util')
+const glob = require('glob')
+var _ = require('underscore');
+var path = require('path')
+var storage = multer.diskStorage(
+    {
+        destination: './uploads/',
+        filename: function (req, file, cb) {
+            //req.body is empty...
+            //How could I get the new_file_name property sent from client here?
+            cb(null, file.originalname);
+        }
+    }
+);
 
+var upload = multer({ storage: storage });
+
+router.route('/upload')
+    .post(upload.single('file'), post);
+
+function post(request, response) {
+
+    response.json({ message: 'Files Uploaded!' });
+
+}
 const app = express()
 
-app.get('/', function (req, res) {
+app.use(express.json())
 
-    res.send('Test')
+
+app.post('/file', upload.single('file') ,function (req, res) {
+
+
+    const body = req.body
+    const file = req.file
+    const fl = req.fl
+    const x = file['filename']
+    console.log(file['filename'])
+
+    res.send('file')
+
 
 })
 
-app.listen(3000)
+
+
+
+
+
+app.get('/' , function (req, res) {
+    var filePath = "/uploads/DGA_2005_informe_gestion_2005.pdf";
+    console.log(filePath)
+    fs.readFile(__dirname + filePath, function (_err, data) {
+        res.contentType("application/pdf");
+        res.send(data);
+
+    });
+});
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, function () {
+    console.log(PORT);
+});
+
